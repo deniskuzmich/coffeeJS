f90a343ed7b0ef91e28e1eec83aeec34240a3c6c
Denis <kuzmichdenis21@gmail.com>
2022-02-02 21:34:59 +0300

    modal window

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,31 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
+
+    const modal = document.querySelector('.modal'),
+          openBtn = document.querySelector('[data-modal'),
+          closeBtn = document.querySelector('.modal__close');
+
+
+    function openModal() {
+        modal.style.display = 'block';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
+    }
+
+    openBtn.addEventListener('click', openModal);
+
+    closeBtn.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal) {
+            closeModal();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' &&  modal.style.display === 'block') {
+            closeModal();
+        }
+    })
 })
